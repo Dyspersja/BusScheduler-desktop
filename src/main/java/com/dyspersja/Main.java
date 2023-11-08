2a12afd79c39248a9a87b25a09245f99c6960d66
Dyspersja <dyspersjamusic@gmail.com>
2023-11-08 18:45:56 +0100

    Renamed main class to Main

--- src/main/java/com/dyspersja/Main.java
+++ src/main/java/com/dyspersja/Main.java
@@ -1,9 +1,7 @@
 package com.dyspersja;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class Main {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
