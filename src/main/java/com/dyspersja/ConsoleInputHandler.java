e325117cf41a9d188e914d1e16ed685e58103629
Dyspersja <dyspersjamusic@gmail.com>
2023-11-08 19:30:13 +0100

    Added ConsoleInputHandler class

---
+++ src/main/java/com/dyspersja/ConsoleInputHandler.java
@@ -0,0 +1,11 @@
+package com.dyspersja;
+
+public class ConsoleInputHandler {
+    public void start() {
+
+    }
+
+    private void runLoop() {
+
+    }
+}
--- src/main/java/com/dyspersja/Main.java
+++ src/main/java/com/dyspersja/Main.java
@@ -2,6 +2,7 @@ package com.dyspersja;
 
 public class Main {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        var inputHandler = new ConsoleInputHandler();
+        inputHandler.start();
     }
 }
