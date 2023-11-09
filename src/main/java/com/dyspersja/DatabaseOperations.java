cc922502d6ef882abd8b2a9575649d8818bf3453
Dyspersja <dyspersjamusic@gmail.com>
2023-11-09 11:44:43 +0100

    Added enum for database operations

---
+++ src/main/java/com/dyspersja/DatabaseOperations.java
@@ -0,0 +1,25 @@
+package com.dyspersja;
+
+public enum DatabaseOperations {
+
+    SELECT("select", "s"),
+    UPDATE("update", "u"),
+    DELETE("delete", "d"),
+    INSERT("insert", "i");
+
+    private final String longForm;
+    private final String shortForm;
+
+    DatabaseOperations(String longForm, String shortForm) {
+        this.longForm = longForm;
+        this.shortForm = shortForm;
+    }
+
+    public String getLongForm() {
+        return longForm;
+    }
+
+    public String getShortForm() {
+        return shortForm;
+    }
+}
