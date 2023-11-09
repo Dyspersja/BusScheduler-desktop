3f55facad4152bb67ee1df44151cd9e27c19cd51
Dyspersja <dyspersjamusic@gmail.com>
2023-11-09 11:32:04 +0100

    Added welcome message at the application start

--- src/main/java/com/dyspersja/ConsoleInputHandler.java
+++ src/main/java/com/dyspersja/ConsoleInputHandler.java
@@ -1,11 +1,23 @@
 package com.dyspersja;
 
+import java.util.Scanner;
+
 public class ConsoleInputHandler {
     public void start() {
+        System.out.println("Welcome to Bus Transport Management application" + "\n" +
+                "Enter name of the operation you want to perform or type " +
+                "'help' for a brief introduction and list of available commands.");
 
+        runLoop();
     }
 
     private void runLoop() {
+        boolean isRunning = true;
+        Scanner input = new Scanner(System.in);
 
+        while(isRunning) {
+            String userInput = input.nextLine();
+            System.out.println(userInput);
+        }
     }
 }
