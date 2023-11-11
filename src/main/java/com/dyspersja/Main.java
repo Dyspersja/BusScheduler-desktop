cc11eac027cf52338c361113c0fb87c9353b8a29
Dyspersja <dyspersjamusic@gmail.com>
2023-11-11 13:44:25 +0100

    Added PropertiesLoader initialization to main method

--- src/main/java/com/dyspersja/Main.java
+++ src/main/java/com/dyspersja/Main.java
@@ -1,7 +1,11 @@
 package com.dyspersja;
 
+import com.dyspersja.properties.PropertiesLoader;
+
 public class Main {
     public static void main( String[] args ) {
+        PropertiesLoader.createInstance(args);
+
         var inputHandler = new ConsoleInputHandler();
         inputHandler.start();
     }
