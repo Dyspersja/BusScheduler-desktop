bdb7f6b57b5a0a6e12763f187c7fbc945eedfb0c
Dyspersja <dyspersjamusic@gmail.com>
2023-11-11 12:41:42 +0100

    Moved code for parsing program arguments to PropertiesArgumentParser class

--- src/main/java/com/dyspersja/properties/PropertiesArgumentParser.java
+++ src/main/java/com/dyspersja/properties/PropertiesArgumentParser.java
@@ -1,4 +1,33 @@
 package com.dyspersja.properties;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
 public class PropertiesArgumentParser {
+
+    @Parameter(names = "--url", description = "Database URL")
+    private String url;
+    @Parameter(names = {"--username", "-u"}, description = "Database username")
+    private String username;
+    @Parameter(names = {"--password", "-p"}, description = "Database password")
+    private String password;
+
+    public PropertiesArgumentParser(String[] args) {
+        JCommander.newBuilder()
+                .addObject(this)
+                .build()
+                .parse(args);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
