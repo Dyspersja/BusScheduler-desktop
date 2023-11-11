93393d5f87e6e6e96c5c7f9343b42748c5f7b7db
Dyspersja <dyspersjamusic@gmail.com>
2023-11-11 10:11:34 +0100

    Added method for retrieving properties from PropertiesFileReader class

--- src/main/java/com/dyspersja/properties/PropertiesFileReader.java
+++ src/main/java/com/dyspersja/properties/PropertiesFileReader.java
@@ -1,4 +1,18 @@
 package com.dyspersja.properties;
 
+import java.util.Properties;
+
 public class PropertiesFileReader {
+    private final String defaultPropertiesFilePath = "application.properties";
+    private final String resourcesPropertiesFilePath = "src/main/resources/application.properties";
+
+    private final Properties properties;
+
+    public PropertiesFileReader() {
+        properties = new Properties();
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
