b2dba3ed2013c17597113cbbd31ff00c2077e551
Dyspersja <dyspersjamusic@gmail.com>
2023-11-11 14:40:01 +0100

    Added ConsoleMessageWriter class for priniting longer console messages

--- src/main/java/com/dyspersja/ConsoleInputHandler.java
+++ src/main/java/com/dyspersja/ConsoleInputHandler.java
@@ -1,32 +1,15 @@
 package com.dyspersja;
 
+import com.dyspersja.console.ConsoleMessageWriter;
 import com.dyspersja.database.DatabaseOperations;
 
 import java.util.Scanner;
 
 public class ConsoleInputHandler {
-    private final String WELCOME_MESSAGE = """
-            Welcome to Bus Transport Management application
-            
-            Enter name of the operation you want to perform
-            type 'help' for a brief introduction and list of available commands.\s
-            type 'exit' to close""";
-
-    private final String HELP_MESSAGE = """
-            Available commands:
-            help - prints a list of commands with a brief description
-            exit - closes the program
-            
-            Available operations:
-            SELECT "select", "s"
-            UPDATE "update", "u"
-            DELETE "delete", "d"
-            INSERT "insert", "i"
-            """;
 
     public void start() {
-        System.out.println(WELCOME_MESSAGE);
-
+        var messageWriter = new ConsoleMessageWriter();
+        messageWriter.printWelcomeMessage();
         runLoop();
     }
 
@@ -38,7 +21,7 @@ public class ConsoleInputHandler {
         while(isRunning) {
             String userInput = input.nextLine();
 
-            if(parser.parseHelpCommand(userInput)) System.out.println(HELP_MESSAGE);
+            if(parser.parseHelpCommand(userInput)) System.out.println("HELP_MESSAGE");
             else if(parser.parseExitCommand(userInput)) {
                 System.out.println("execute exit command");
                 isRunning = false;
---
+++ src/main/java/com/dyspersja/console/ConsoleMessageWriter.java
@@ -0,0 +1,31 @@
+package com.dyspersja.console;
+
+public class ConsoleMessageWriter {
+
+    public void printWelcomeMessage() {
+        final String welcomeMessage = """
+            Welcome to Bus Transport Management application
+            
+            Enter name of the operation you want to perform
+            type 'help' for a brief introduction and list of available commands.\s
+            type 'exit' to close""";
+
+        System.out.println(welcomeMessage);
+    }
+
+    public void printHelpMessage() {
+        final String helpMessage = """
+            Available commands:
+            help - prints a list of commands with a brief description
+            exit - closes the program
+            
+            Available operations:
+            SELECT "select", "s" - Retrieve data from the database
+            UPDATE "update", "u" - Modify existing data in the database
+            DELETE "delete", "d" - Remove data from the database
+            INSERT "insert", "i" - Add new data to the database
+            """;
+
+        System.out.println(helpMessage);
+    }
+}
