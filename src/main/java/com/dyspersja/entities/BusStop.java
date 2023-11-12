4a29850136947a892764cb483ad2d896be4549f8
Dyspersja <dyspersjamusic@gmail.com>
2023-11-12 18:52:03 +0100

    Updated BuStop class

--- src/main/java/com/dyspersja/entities/BusStop.java
+++ src/main/java/com/dyspersja/entities/BusStop.java
@@ -1,4 +1,18 @@
 package com.dyspersja.entities;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BusStop {
+
+    private int id;
+    private String number;
+    private String city;
+    private String street;
+    private double latitude;
+    private double longitude;
 }
