be8f9e2728231a459072e2c721438d43220a3bf2
Dyspersja <dyspersjamusic@gmail.com>
2023-11-14 16:36:37 +0100

    Added repository implementation

--- src/main/java/com/dyspersja/database/tables/TableRepository.java
+++ src/main/java/com/dyspersja/database/tables/TableRepository.java
@@ -1,4 +1,14 @@
 package com.dyspersja.database.tables;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
 public interface TableRepository<T> {
+    void deleteById(int id) throws SQLException;
+    boolean existsById(int id) throws SQLException;
+    List<T> findAll() throws SQLException;
+    Optional<T> findById(int id) throws SQLException;
+    void save(T entity) throws SQLException;
+    void update(T entity) throws SQLException;
 }
