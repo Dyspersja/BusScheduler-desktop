7f596cba48579c668e17fff1c4355968121ac1a6
Dyspersja <dyspersjamusic@gmail.com>
2023-12-12 18:14:23 +0100

    Added menubar to frame

--- src/main/java/com/dyspersja/window/components/MainFrame.java
+++ src/main/java/com/dyspersja/window/components/MainFrame.java
@@ -1,11 +1,18 @@
 package com.dyspersja.window.components;
 
+import com.dyspersja.window.components.menu.MainMenuBar;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
 
+    private final MainMenuBar menuBar;
+
     public MainFrame() {
+        menuBar = new MainMenuBar();
+        setJMenuBar(menuBar);
+
         setLayout(new BorderLayout());
         setSize(700,400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
