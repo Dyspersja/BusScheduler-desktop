e734bbb22f385bf1dab676f253635265f94e8660
Dyspersja <dyspersjamusic@gmail.com>
2024-01-01 11:16:10 +0100

    Added CityDistrict class

---
+++ src/main/java/com/dyspersja/model/CityDistrict.java
@@ -0,0 +1,28 @@
+package com.dyspersja.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "city_districts")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CityDistrict {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "district_name", nullable = false, unique = true)
+    private String districtName;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_zone_id")
+    private TicketZone ticketZone;
+}
