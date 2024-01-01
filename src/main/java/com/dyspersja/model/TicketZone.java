e3532d2bba6f10e689c909620e0e980f79a86099
Dyspersja <dyspersjamusic@gmail.com>
2024-01-01 11:11:53 +0100

    Added TicketZone class

---
+++ src/main/java/com/dyspersja/model/TicketZone.java
@@ -0,0 +1,21 @@
+package com.dyspersja.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "ticket_zones")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketZone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "zone_name", nullable = false, unique = true)
+    private String zoneName;
+}
