514ad62a548be5350ae0777c5a2d87bdd300a3c0
Dyspersja <dyspersjamusic@gmail.com>
2024-01-02 12:15:24 +0100

    Added exit button

--- src/main/java/com/dyspersja/window/components/menu/FileMenu.java
+++ src/main/java/com/dyspersja/window/components/menu/FileMenu.java
@@ -5,8 +5,18 @@ import java.awt.event.KeyEvent;
 
 public class FileMenu extends JMenu {
 
+    private final JMenuItem exitMenuItem;
+
     public FileMenu() {
         setText("File");
         setMnemonic(KeyEvent.VK_F);
+
+        this.exitMenuItem = new JMenuItem("Exit");
+        initializeExitMenuItem();
+    }
+
+    private void initializeExitMenuItem() {
+        exitMenuItem.addActionListener(e -> System.exit(0));
+        add(exitMenuItem);
     }
 }
