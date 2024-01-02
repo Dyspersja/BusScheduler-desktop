2c27ee01243d2b1f9431b92d4396ae93a0ab6160
Dyspersja <dyspersjamusic@gmail.com>
2024-01-02 10:18:19 +0100

    Added repository classes for all tables

---
+++ src/main/java/com/dyspersja/database/tables/busstop/BusStopRepository.java
@@ -0,0 +1,10 @@
+package com.dyspersja.database.tables.busstop;
+
+import com.dyspersja.database.tables.TableRepository;
+
+public class BusStopRepository extends TableRepository<BusStop> {
+
+    public BusStopRepository() {
+        super(BusStop.class);
+    }
+}
