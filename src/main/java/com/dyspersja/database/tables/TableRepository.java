64aba94df598d8c361b65845936a688589fc7cd9
Dyspersja <dyspersjamusic@gmail.com>
2024-01-02 10:12:55 +0100

    Changed TableRepository interface to class added implementation and used generics

--- src/main/java/com/dyspersja/database/tables/TableRepository.java
+++ src/main/java/com/dyspersja/database/tables/TableRepository.java
@@ -1,13 +1,65 @@
 package com.dyspersja.database.tables;
 
+import com.dyspersja.database.DatabaseSessionManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
-public interface TableRepository<T> {
-    void save(T entity) throws HibernateException;
-    T getById(Long id) throws HibernateException;
-    List<T> getAll() throws HibernateException;
-    void update(T entity) throws HibernateException;
-    void delete(Long id) throws HibernateException;
+public class TableRepository<T> {
+
+    private final DatabaseSessionManager sessionManager;
+    private final Class<T> entityClass;
+
+    public TableRepository(Class<T> entityClass) {
+        sessionManager = DatabaseSessionManager.getInstance();
+        this.entityClass = entityClass;
+    }
+
+    public void save(T entity) throws HibernateException {
+        try (Session session = sessionManager.getNewSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        }
+    }
+
+    public T getById(Long id) throws HibernateException {
+        try (Session session = sessionManager.getNewSession()) {
+            return session.get(entityClass, id);
+        }
+    }
+
+    public List<T> getAll() throws HibernateException {
+        try (Session session = sessionManager.getNewSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<T> query = builder.createQuery(entityClass);
+            Root<T> root = query.from(entityClass);
+            query.select(root);
+            return session.createQuery(query).getResultList();
+        }
+    }
+
+    public void update(T entity) throws HibernateException {
+        try (Session session = sessionManager.getNewSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.merge(entity);
+            transaction.commit();
+        }
+    }
+
+    public void delete(Long id) throws HibernateException {
+        try (Session session = sessionManager.getNewSession()) {
+            Transaction transaction = session.beginTransaction();
+            T entity = session.get(entityClass, id);
+            if (entity != null) {
+                session.remove(entity);
+                transaction.commit();
+            }
+        }
+    }
 }
--- src/main/java/com/dyspersja/database/tables/ticketzone/TicketZoneRepository.java
+++ src/main/java/com/dyspersja/database/tables/ticketzone/TicketZoneRepository.java
@@ -1,69 +1,10 @@
 package com.dyspersja.database.tables.ticketzone;
 
-import com.dyspersja.database.DatabaseSessionManager;
 import com.dyspersja.database.tables.TableRepository;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 
-import java.util.List;
-
-public class TicketZoneRepository implements TableRepository<TicketZone> {
-
-    private final DatabaseSessionManager sessionManager;
+public class TicketZoneRepository extends TableRepository<TicketZone> {
 
     public TicketZoneRepository() {
-        sessionManager = DatabaseSessionManager.getInstance();
-    }
-
-    @Override
-    public void save(TicketZone ticketZone) throws HibernateException {
-        try (Session session = sessionManager.getNewSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.persist(ticketZone);
-            transaction.commit();
-        }
-    }
-
-    @Override
-    public TicketZone getById(Long id) throws HibernateException {
-        try (Session session = sessionManager.getNewSession()) {
-            return session.get(TicketZone.class, id);
-        }
-    }
-
-    @Override
-    public List<TicketZone> getAll() throws HibernateException {
-        try (Session session = sessionManager.getNewSession()) {
-            CriteriaBuilder builder = session.getCriteriaBuilder();
-            CriteriaQuery<TicketZone> query = builder.createQuery(TicketZone.class);
-            Root<TicketZone> root = query.from(TicketZone.class);
-            query.select(root);
-            return session.createQuery(query).getResultList();
-        }
-    }
-
-    @Override
-    public void update(TicketZone ticketZone) throws HibernateException {
-        try (Session session = sessionManager.getNewSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.merge(ticketZone);
-            transaction.commit();
-        }
-    }
-
-    @Override
-    public void delete(Long id) throws HibernateException {
-        try (Session session = sessionManager.getNewSession()) {
-            Transaction transaction = session.beginTransaction();
-            TicketZone ticketZone = session.get(TicketZone.class, id);
-            if (ticketZone != null) {
-                session.remove(ticketZone);
-                transaction.commit();
-            }
-        }
+        super(TicketZone.class);
     }
 }
